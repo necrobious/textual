ffe81f76b45bc5671e0f4ffac5a633867205d3a2
Kirk Peterson <necrobious@gmail.com>
2013-05-28 12:36:33 -0700

    minor refactor, added small description to README

--- README.md
+++ README.md
@@ -1,4 +1,16 @@
-textual
+Data.Textual
 =======
 
 A simple Type Class for converting textual datatypes into other textual datatypes.
+
+Textual simply implements the boilerplate for converting text of one type to another.
+We introduce a a type-class to wrangle the conversion permutatons under.
+
+Current supported textual types:
+  String
+  Data.Text
+  Data.Text.Lazy
+  ByteString (UTF-8) 
+  ByteString.Lazy (UTF-8) 
+
+
--- src/Data/Textual.hs
+++ src/Data/Textual.hs
@@ -19,38 +19,38 @@ class Textual a where
   toUtf8LBS  :: a -> LazyByteString.ByteString
 
 instance Textual StrictText.Text where
-  toLazyText strictText = LazyText.fromChunks [strictText] 
-  toText     strictText = strictText 
-  toString   strictText = StrictText.unpack strictText 
-  toUtf8BS   strictText = StrictTextEncoding.encodeUtf8 strictText 
-  toUtf8LBS  strictText = LazyTextEncoding.encodeUtf8 (toLazyText strictText) 
+  toLazyText = LazyText.fromChunks . (:[]) 
+  toText     = id 
+  toString   = StrictText.unpack 
+  toUtf8BS   = StrictTextEncoding.encodeUtf8  
+  toUtf8LBS  = LazyTextEncoding.encodeUtf8 . toLazyText
 
 instance Textual LazyText.Text where
-  toLazyText lazyText   = lazyText
-  toText     lazyText   = StrictText.concat (LazyText.toChunks lazyText)
-  toString   lazyText   = LazyText.unpack lazyText
-  toUtf8BS   lazyText   = StrictTextEncoding.encodeUtf8 (toText lazyText) 
-  toUtf8LBS  lazyText   = LazyTextEncoding.encodeUtf8 lazyText
+  toLazyText = id 
+  toText     = StrictText.concat . LazyText.toChunks
+  toString   = LazyText.unpack 
+  toUtf8BS   = StrictTextEncoding.encodeUtf8 . toText
+  toUtf8LBS  = LazyTextEncoding.encodeUtf8
 
 instance Textual String where
-  toLazyText string     = LazyText.pack string
-  toText     string     = StrictText.pack string
-  toString   string     = string
-  toUtf8BS   string     = StrictChar8.pack string 
-  toUtf8LBS  string     = LazyChar8.pack string
+  toLazyText = LazyText.pack
+  toText     = StrictText.pack
+  toString   = id
+  toUtf8BS   = StrictChar8.pack 
+  toUtf8LBS  = LazyChar8.pack
 
 instance Textual StrictByteString.ByteString where
-  toLazyText strictBs   = LazyTextEncoding.decodeUtf8 (toUtf8LBS strictBs) 
-  toText     strictBs   = StrictTextEncoding.decodeUtf8 strictBs   
-  toString   strictBs   = StrictChar8.unpack strictBs
-  toUtf8BS   strictBs   = strictBs
-  toUtf8LBS  strictBs   = LazyByteString.fromChunks [strictBs]
+  toLazyText = LazyTextEncoding.decodeUtf8 . toUtf8LBS
+  toText     = StrictTextEncoding.decodeUtf8
+  toString   = StrictChar8.unpack
+  toUtf8BS   = id 
+  toUtf8LBS  = LazyByteString.fromChunks . (:[]) 
 
 instance Textual LazyByteString.ByteString where
-  toLazyText lazyBs     = LazyTextEncoding.decodeUtf8 lazyBs
-  toText     lazyBs     = StrictTextEncoding.decodeUtf8 (toUtf8BS lazyBs)
-  toString   lazyBs     = LazyChar8.unpack lazyBs    
-  toUtf8BS   lazyBs     = StrictByteString.concat (LazyByteString.toChunks lazyBs)
-  toUtf8LBS  lazyBs     = lazyBs
+  toLazyText = LazyTextEncoding.decodeUtf8
+  toText     = StrictTextEncoding.decodeUtf8 . toUtf8BS
+  toString   = LazyChar8.unpack
+  toUtf8BS   = StrictByteString.concat . LazyByteString.toChunks
+  toUtf8LBS  = id
 
 
